aa6816a96e12d8d2995a8dc04ae0d6e520bb3d03
NMakarevich <nikoladze529@mail.ru>
2025-03-14 21:02:40 +0300

    refactor: add button to show/hide password

--- src/shared/ui/form/form-field.tsx
+++ src/shared/ui/form/form-field.tsx
@@ -1,29 +1,63 @@
+import { useState } from 'react';
 import { InputFieldProps } from '@shared/index';
 
 import styles from './form-field.module.scss';
 
 export function FormField(props: InputFieldProps) {
-  return (
-    <div className={styles.formField}>
-      {props.type === 'checkbox' ? (
+  const [showPassword, setShowPassword] = useState(false);
+
+  function handleShowPassword() {
+    setShowPassword(!showPassword);
+  }
+
+  if (props.type === 'password') {
+    return (
+      <div className={styles.formField}>
+        <label htmlFor={props.id}>{props.label}</label>
+        <div className={styles.formFieldPassword}>
+          <input
+            {...props}
+            type={showPassword ? 'text' : 'password'}
+            {...props.register}
+          />
+          <button
+            className={`${styles.formFieldPasswordButton} ${
+              showPassword
+                ? styles.formFieldPasswordShow
+                : styles.formFieldPasswordHide
+            }`}
+            type={'button'}
+            onClick={handleShowPassword}
+          ></button>
+        </div>
+        <span className={styles.formFieldError}>{props.error}</span>
+      </div>
+    );
+  }
+
+  if (props.type === 'checkbox') {
+    return (
+      <div className={styles.formField}>
         <div className={styles.formFieldCheckbox}>
           <input {...props} {...props.register} />
           <label htmlFor={props.id}>{props.label}</label>
         </div>
-      ) : (
-        <>
-          <label htmlFor={props.id}>{props.label}</label>
-          <input {...props} {...props.register} />
-          {props.datalist && (
-            <datalist id={props.list}>
-              {props.datalist.map((item) => (
-                <option value={item} key={item}>
-                  {item}
-                </option>
-              ))}
-            </datalist>
-          )}
-        </>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.formField}>
+      <label htmlFor={props.id}>{props.label}</label>
+      <input {...props} {...props.register} />
+      {props.datalist && (
+        <datalist id={props.list}>
+          {props.datalist.map((item) => (
+            <option value={item} key={item}>
+              {item}
+            </option>
+          ))}
+        </datalist>
       )}
       <span className={styles.formFieldError}>{props.error}</span>
     </div>
